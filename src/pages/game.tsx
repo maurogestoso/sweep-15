901cd28f6846d9537bf84f7de3e538d2c6f7877c
Mauro Gestoso <maurogestoso@gmail.com>
2022-09-15 11:22:37 +0200

    Move game logic to separate page

---
+++ src/pages/game.tsx
@@ -0,0 +1,63 @@
+import type { NextPage } from "next";
+import { useMachine } from "@xstate/react";
+import { gameMachine } from "../state";
+import { Card } from "../card";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  const [current, send] = useMachine(gameMachine);
+  const { context: ctx } = current;
+  return (
+    <>
+      <main className="container mx-auto h-screen">
+        <h1 className="font-bold text-3xl">Sweep 15</h1>
+        <section className="border border-black">
+          <p>{`Current state: ${current.value}`}</p>
+        </section>
+        <section className="border border-black h-[500px]">
+          {current.matches("ready to start") && (
+            <button onClick={() => send("start")}>Shuffle and deal</button>
+          )}
+          {current.matches("player turn") && (
+            <div>
+              <div>
+                <h3 className="font-bold text-2xl">Table</h3>
+                <p>{`Deck: ${ctx.deck.length}`}</p>
+                {ctx.table.map((card, i) => (
+                  <Card key={i} {...card} />
+                ))}
+              </div>
+              <div className="flex justify-between">
+                <div>
+                  <h3 className="font-bold text-2xl">Player 1</h3>
+                  {ctx.players[0]?.hand.map((card, i) => (
+                    <Card key={i} {...card} />
+                  ))}
+                </div>
+                <div>
+                  <h3 className="font-bold text-2xl">Player 2</h3>
+                  {ctx.players[1]?.hand.map((card, i) => (
+                    <Card key={i} {...card} />
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Home;
+
+function Card({ imgUrl, value, suit }: Card) {
+  return (
+    <Image
+      src={imgUrl}
+      alt={`${value} of ${suit} card`}
+      width={140}
+      height={190}
+    />
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useMachine } from "@xstate/react";
-import { gameMachine } from "../state";
-import { Card } from "../card";
-import Image from "next/image";
 import { PreloadImages } from "../components/PreloadImages";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  const [current, send] = useMachine(gameMachine);
-  const { context: ctx } = current;
   return (
     <>
       <Head>
@@ -20,53 +15,12 @@ const Home: NextPage = () => {
 
       <main className="container mx-auto h-screen">
         <h1 className="font-bold text-3xl">Sweep 15</h1>
-        <section className="border border-black">
-          <p>{`Current state: ${current.value}`}</p>
-        </section>
-        <section className="border border-black h-[500px]">
-          {current.matches("Ready to start") && (
-            <button onClick={() => send("start")}>Start Game</button>
-          )}
-          {current.matches("player turn") && (
-            <div>
-              <div>
-                <h3 className="font-bold text-2xl">Table</h3>
-                <p>{`Deck: ${ctx.deck.length}`}</p>
-                {ctx.table.map((card, i) => (
-                  <Card key={i} {...card} />
-                ))}
-              </div>
-              <div className="flex justify-between">
-                <div>
-                  <h3 className="font-bold text-2xl">Player 1</h3>
-                  {ctx.players[0]?.hand.map((card, i) => (
-                    <Card key={i} {...card} />
-                  ))}
-                </div>
-                <div>
-                  <h3 className="font-bold text-2xl">Player 2</h3>
-                  {ctx.players[1]?.hand.map((card, i) => (
-                    <Card key={i} {...card} />
-                  ))}
-                </div>
-              </div>
-            </div>
-          )}
-        </section>
+        <Link href="/game">
+          <a>Start Game</a>
+        </Link>
       </main>
     </>
   );
 };
 
 export default Home;
-
-function Card({ imgUrl, value, suit }: Card) {
-  return (
-    <Image
-      src={imgUrl}
-      alt={`${value} of ${suit} card`}
-      width={140}
-      height={190}
-    />
-  );
-}
