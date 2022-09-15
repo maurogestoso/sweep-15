57a228a7177705543430436985391f0eb26c3a27
Mauro Gestoso <maurogestoso@gmail.com>
2022-09-15 11:44:51 +0200

    Add selected cards state

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,6 +1,6 @@
 import { assign, createMachine } from "xstate";
 import { baseDeck, shuffleDeck } from "../card";
-import type { Card } from "../card";
+import type { Card, CardId } from "../card";
 
 export interface GameContext {
   roundsLeft: number;
@@ -9,6 +9,10 @@ export interface GameContext {
   players: {
     name: string;
     hand: Card[];
+    selected: {
+      hand: CardId | null;
+      table: Set<CardId>;
+    };
   }[];
 }
 
@@ -23,103 +27,110 @@ export type GameEvents =
 export const gameMachine =
   /** @xstate-layout N4IgpgJg5mDOIC5QGUDuYwAcAEBGArAHQBOYAhhAJ7YAuA9trDWcTQMRMs2KiZ2wBLGgLoA7HiAAeiALQAmAMwBOQgAZcSgCwA2bQA5lC-Du0AaEJUSK9hXLkWalqhdvxatAXw-m0GHAUJMABsySjBiWgBXYlEOdCxsAGMWCHgkED5BYTEJaQRNfBttVQB2JTkSzTt8BQU9c0sEBVwFQgLcPU0SuqV8OT0lLx94-yJg0PComLYIYjocZOIICUyhEXF0vM05OTUFdRLcXVVtJWV8BsRywmLOpQH1VU65bSGQXwSA8bCImmjY+hQKBBMDYAAWZFEECSKRW-DWOU2V1whHwNQUJV0+AI3R2lwQ2kI+3Rjm0GiUxUcbw+o0CIR+UwBdCBINoZAARqzFst0qtshtQHklCjumUSodtJjhRV8bgnoQ9Iquvs5EdcCU3NSRngxvTJn9pnCsutcogCLtapplKoauLeuL8fhMYQKk7HE8nHIlCUtX4dSQ6JEodg6AA3cJsURgSQ0bBzIM83jw-mmhAybSaNQFQ5y7QYgh5-G3Qga5rlSqlPTOX2fIjx4NhiNGhECqSIIwlEsdeyFbGaAwlfHdVovPMasquDWvN6iOgQOASGn+0gUaj0RjMVjNlNItM7Qmq1QvE59AwKfuy-o3G2nDMDDMYuQ12nffX-bcm3dehXivSuCl6Kq56dPichorY5rYkYf5Yj63jvNqAT1tCjbEB+iKCma1xyDamj9gochWk4VpDhULr-mSLz4Lo9zPv6UBkAAtqCqHoa2eTaDsRLqOqkp9gRFwWIgdoutBnFOt0uh6HRBBsam8h5moGgmAYZzGLo+LpkQWJOko5TKB0+heF4QA */
   createMachine(
-  {
-    id: "Sweep 15",
+    {
+      id: "Sweep 15",
+
     // tsTypes: {} as import("./index.typegen").Typegen0,
-    predictableActionArguments: true,
-    schema: {
-      context: {} as GameContext,
-      events: {} as GameEvents,
-    },
-    context: {
-      roundsLeft: 3,
-      deck: baseDeck,
-      table: [],
-      players: [
-        { name: "Player 1", hand: [] },
-        { name: "Player 2", hand: [] },
-      ],
-    },
-    initial: "ready to start",
-    states: {
-      "ready to start": {
-        on: {
-          start: {
-            actions: ["setUpRoundStart", "shuffleAndDeal", "setPlayerOrder"],
-            target: "player turn",
+      predictableActionArguments: true,
+
+      context: {
+        roundsLeft: 3,
+        deck: baseDeck,
+        table: [],
+        players: [
+          {
+            name: "Player 2",
+            hand: [],
+            selected: { hand: null, table: new Set<CardId>() },
           },
-        },
+          {
+            name: "Player 1",
+            hand: [],
+            selected: { hand: null, table: new Set<CardId>() },
+          },
+        ],
       },
-      "player turn": {
-        entry: "determineActivePlayer",
-        always: {
-          cond: "isRoundOver",
-          target: "round over",
-        },
-        on: {
-          "sweep cards": {
-            actions: "sweepCards",
-            cond: "setAdds15",
-          },
-          "drop card": {
-            actions: "dropCard",
-            cond: "isHandCardSelected",
-          },
-          "toggle hand card": {
-            actions: "toggleHandCard",
-          },
-          "toggle table card": {
-            actions: "toggleTableCard",
+
+      initial: "ready to start",
+      states: {
+        "ready to start": {
+          on: {
+            start: {
+              actions: ["setUpRoundStart", "shuffleAndDeal", "setPlayerOrder"],
+              target: "player turn",
+            },
           },
         },
+        "player turn": {
+          entry: "determineActivePlayer",
+          always: {
+            cond: "isRoundOver",
+            target: "round over",
+          },
+          on: {
+            "sweep cards": {
+              actions: "sweepCards",
+              cond: "setAdds15",
+            },
+            "drop card": {
+              actions: "dropCard",
+              cond: "isHandCardSelected",
+            },
+            "toggle hand card": {
+              actions: "toggleHandCard",
+            },
+            "toggle table card": {
+              actions: "toggleTableCard",
+            },
+          },
+        },
+        "round over": {
+          always: {
+            cond: "isGameOver",
+            target: "game over",
+          },
+          on: {
+            "next round": {
+              actions: "setUpRoundStart",
+              target: "player turn",
+            },
+          },
+        },
+        "game over": {},
       },
-      "round over": {
-        always: {
-          cond: "isGameOver",
-          target: "game over",
-        },
-        on: {
-          "next round": {
-            actions: "setUpRoundStart",
-            target: "player turn",
-          },
-        },
+    },
+    {
+      guards: {
+        isRoundOver: () => false,
       },
-      "game over": {},
-    },
-  },
-  {
-    guards: {
-      isRoundOver: () => false,
-    },
-    actions: {
-      setUpRoundStart: assign((ctx) => {
-        const newPlayers = ctx.players;
-        const newTable = ctx.table;
-        const deck = shuffleDeck(ctx.deck);
+      actions: {
+        setUpRoundStart: assign((ctx) => {
+          const newPlayers = ctx.players;
+          const newTable = ctx.table;
+          const deck = shuffleDeck(ctx.deck);
 
-        for (let i = 0; i < 3; i += 1) {
-          let topCard = deck.shift();
-          if (!topCard) break;
-          ctx.players[0]?.hand.push(topCard);
+          for (let i = 0; i < 3; i += 1) {
+            let topCard = deck.shift();
+            if (!topCard) break;
+            ctx.players[0]?.hand.push(topCard);
 
-          topCard = deck.shift();
-          if (!topCard) break;
-          ctx.players[1]?.hand.push(topCard);
-        }
+            topCard = deck.shift();
+            if (!topCard) break;
+            ctx.players[1]?.hand.push(topCard);
+          }
 
-        for (let i = 0; i < 4; i += 1) {
-          const topCard = deck.shift();
-          if (!topCard) break;
-          newTable.push(topCard);
-        }
+          for (let i = 0; i < 4; i += 1) {
+            const topCard = deck.shift();
+            if (!topCard) break;
+            newTable.push(topCard);
+          }
 
-        return {
-          deck,
-          players: newPlayers,
-          table: newTable,
-        };
-      }),
-    },
-  }
-);
+          return {
+            deck,
+            players: newPlayers,
+            table: newTable,
+          };
+        }),
+      },
+    }
+  );
